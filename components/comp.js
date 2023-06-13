f7dee0b755421151a71d49a49b5b7b5db59b85cc
rohit-siva-sai <106007387+rohit-siva-sai@users.noreply.github.com>
2023-06-13 19:38:19 +0530

    initial commit

--- components/comp.js
+++ components/comp.js
@@ -5,7 +5,7 @@ const Comp = ({img,head,text}) => {
     <div className='border shadow-md flex flex-col items-center space-y-2 rounded-tr-[60px] w-72 px-4
      rounded-bl-[60px] bg-white py-4 ' >
       <picture>
-        <img src={img} alt="" className=' rounded-tr-[60px]  rounded-bl-[60px] w-64 h-64' />
+        <img src={img} alt="" className=' rounded-tr-[60px]   w-64 h-64' />
       </picture>
       <p className='font-bold text-xl uppercase'>{head}</p>
       <p className='text-center'>{text}</p>
